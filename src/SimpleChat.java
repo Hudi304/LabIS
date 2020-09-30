47403487b3bc991d8e5a20adbaeb3c219f088927
Mihai Hudisteanu <hudisteanum@yahoo.com>
2020-09-30 22:25:56 +0300

    Dani

---
+++ src/SimpleChat.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)SimpleChat.java
+ *
+ * SimpleChat application
+ *
+ * @author 
+ * @version 1.00 2020/9/29
+ */
+import com.lloseng.ocsf.client.*;
+public class SimpleChat {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
